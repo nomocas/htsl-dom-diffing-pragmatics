a182225acb67976ad85b84659fa8b5cc53ba6a78
Gilles Coomans <gilles.coomans@gmail.com>
2017-04-12 01:09:42 +0200

    fix(build): clean rollup.config

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,28 @@
 import babel from 'rollup-plugin-babel';
 import babelrc from 'babelrc-rollup';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
-let pkg = require('./package.json');
-let external = Object.keys(pkg.dependencies);
+
+const pkg = require('./package.json');
+const external = Object.keys(pkg.dependencies);
 
 export default {
-  entry: 'src/index.js',
-  plugins: [ babel(babelrc()) ],
-  external: external,
-  targets: [
-    {
-      dest: pkg.main,
-      format: 'umd',
-      moduleName: '__MY__PROJECT__',
-      sourceMap: true
-    },
-    {
-      dest: pkg.module,
-      format: 'es',
-      sourceMap: true
-    }
-  ]
+	entry: 'src/index.js',
+	plugins: [babel(babelrc()), nodeResolve(), commonjs({
+		namedExports: {
+			'postal/lib/postal.lodash.js': ['postal']
+		}
+	})],
+	external,
+	targets: [{
+		dest: pkg.main,
+		format: 'cjs',
+		sourceMap: true
+	}, {
+		dest: pkg.module,
+		format: 'es',
+		sourceMap: true
+	}]
 };
+
